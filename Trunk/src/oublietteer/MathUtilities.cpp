5afbc93d84fae8a1550b0c4e4a38145cdc4c576d
Pierce Lewis Brooks <piercebrooks@rocketmail.com>
2022-09-29 17:53:16 -0400

    eliminate math constant interdependencies

--- Trunk/src/oublietteer/MathUtilities.cpp
+++ Trunk/src/oublietteer/MathUtilities.cpp
@@ -12,15 +12,15 @@ const float oublietteer::util::EULER = 2.718281828f;
 
 const float oublietteer::util::PI = 3.14159256359f;
 
-const float oublietteer::util::HALF_PI = PI*0.5f;
+const float oublietteer::util::HALF_PI = 3.14159256359f*0.5f;
 
-const float oublietteer::util::QUARTER_PI = HALF_PI*0.5f;
+const float oublietteer::util::QUARTER_PI = 3.14159256359f*0.25f;
 
-const float oublietteer::util::TWO_PI = PI*2.0f;
+const float oublietteer::util::TWO_PI = 3.14159256359f*2.0f;
 
-const float oublietteer::util::RAD_TO_DEG = 180.0f/PI;
+const float oublietteer::util::RAD_TO_DEG = 180.0f/3.14159256359f;
 
-const float oublietteer::util::DEG_TO_RAD = PI/180.0f;
+const float oublietteer::util::DEG_TO_RAD = 3.14159256359f/180.0f;
 
 const float oublietteer::util::INFIN = std::numeric_limits<float>::infinity();
 
